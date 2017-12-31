33559df5803699af22cc92b9379ccedb53ac3f03
Damodar Lohani <dlohani48@gmail.com>
2017-12-31 11:23:39 +0545

    menu icon for each screen of drawer navigator

--- src/navigators/navigators.js
+++ src/navigators/navigators.js
@@ -2,17 +2,23 @@ import React from 'react'
 import { StackNavigator, DrawerNavigator } from 'react-navigation';
 import { HomeScreen, ComponentListScreen, LoginScreen, SignupScreen, ProfileScreen, EditProfileScreen }  from '../screens';
 import { DrawerMenu } from '../components'
+import { Icon } from '../components'
 import globalStyles from '../styles/GlobalStyles';
+
+const handleMenuPress = (navigation) => {
+    navigation.navigate('DrawerToggle')
+}
 
 const stackNavOptions = {
     navigationOptions: ({navigation})=>({
         headerStyle: globalStyles.headerStyle,
-        headerTitleStyle: globalStyles.headerTitleStyle
+        headerTitleStyle: globalStyles.headerTitleStyle,
+        headerLeft: <Icon style={{marginLeft: 20}} size={27} onPress={()=>handleMenuPress(navigation)} color="#fff" name="menu" />,
     }),
     headerType: 'screen',
     cardStyle: {
         backgroundColor: '#fff',
-    }
+    },
 };
 
 export const MainStack = DrawerNavigator({
